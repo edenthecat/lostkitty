e324871192369abf4b97cd8c29d0ee6062a5be52
Eden Rohatensky <edenrohatensky@gmail.com>
2017-07-04 14:43:21 -0400

    Fleshed out create and new methods

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -7,9 +7,24 @@ class PetsController < ApplicationController
     @pet = Pet.find(params[:id])
   end
 
+  def new
+    @pet = Pet.new()
+  end
+
+  def create
+    @pet = Pet.create(pet_params)
+    redirect_to pet_path(@pet)
+  end
+
   def destroy
     @pet = Pet.find(params[:id])
     @pet.destroy
     redirect_to pets_path
+  end
+
+  private
+
+  def pet_params
+    params.required(:pet).permit(:name, :address, :category)
   end
 end
